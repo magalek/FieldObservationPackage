aebafdd6743ef511f131bf3d966d11f6052ba269
Filip Walczak <filipwalczak98@gmail.com>
2021-05-30 19:12:15 +0200

    Init commit

---
+++ Editor/FieldObserverWindow.cs
@@ -0,0 +1,82 @@
+﻿using FieldObservationPackage.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+namespace FieldObservationPackage.Editor {
+
+    [EditorWindowTitle(title = "FieldObserver")]
+    public class FieldObserverWindow : EditorWindow {
+
+        private List<FieldInfo> observedFields = new List<FieldInfo>();
+
+        [MenuItem("Windows/FieldObserverWindow")]
+        public static void ShowWindow() {
+            GetWindow<FieldObserverWindow>("FieldObserver");
+            FindObservedFields();
+        }
+
+        private static void FindObservedFields() {
+            Assembly asm = Assembly.GetCallingAssembly();
+            Type[] types = asm.GetTypes();
+
+            foreach (var type in types) {
+                FieldInfo[] fields = type.GetFields();
+                foreach (var field in fields) {
+                    ObserveFieldAttribute attribute = field.GetCustomAttribute<ObserveFieldAttribute>();
+                    if (attribute != null) {
+                        Debug.Log(field.Name);
+                    }
+                }
+                
+            }
+        }
+
+        private void Update() {
+            Repaint();
+        }
+
+        private void OnGUI() {
+            if (!Application.isPlaying) {
+                return;
+            }
+            Profiler.BeginSample("WINDOW GUI");
+            EditorGUILayout.BeginVertical();
+            foreach (ObservedObjectData data in ObserverController.Instance.observedObjectData) {
+                EditorGUILayout.LabelField($"{data.Name} - {data.ObjectType}");
+                foreach (var fieldData in data.Fields) {
+                    EditorGUILayout.BeginHorizontal();
+                    EditorGUI.indentLevel++;
+                    EditorGUILayout.LabelField(fieldData.Info.Name);
+                    object value = fieldData.Info.GetValue(fieldData.ReflectedObject);
+                    var defaultColor = GUI.color;
+                    if (fieldData.ExpectedValue != null) {
+                        bool equal;
+                        try {
+                            equal = Convert.ChangeType(value, fieldData.ExpectedValueType).Equals(Convert.ChangeType(fieldData.ExpectedValue, fieldData.ExpectedValueType));
+                        }
+                        catch (Exception) {
+
+                            throw;
+                        }
+                        GUI.color = equal ? Color.green : Color.red;
+                    }
+                    
+                    EditorGUILayout.LabelField(value.ToString());
+                    GUI.color = defaultColor;
+                    EditorGUI.indentLevel--;
+                    EditorGUILayout.EndHorizontal();
+                }
+                
+            }
+            EditorGUILayout.EndVertical();
+            Profiler.EndSample();
+        }
+    }
+}
---
+++ Runtime/ObserverController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace FieldObservationPackage.Runtime {
+    public class ObserverController : MonoBehaviour {
+
+        public List<ObservedObjectData> observedObjectData = new List<ObservedObjectData>();
+
+        public static ObserverController Instance;
+
+        private void Awake() {
+
+            if (Instance == null) {
+                Instance = this;
+            }
+            else {
+                if (Instance != this) {
+                    Destroy(this);
+                }
+            }
+        }
+
+        private void AddObservedObject(string name, object observedObject) {
+            observedObjectData.Add(PopulateList(name, observedObject));
+        }
+
+        public static void ObserveObject(string name, object observedObject) {
+            if (!Instance) {
+                var go = new GameObject("ObserverController");
+                go.AddComponent<ObserverController>();
+            }
+            Instance.AddObservedObject(name, observedObject);
+        }
+
+            private ObservedObjectData PopulateList(string name, object observedObject) {
+            List<ObservedFieldData> tempList = new List<ObservedFieldData>();
+            Type objectType = observedObject.GetType();
+            FieldInfo[] fields = objectType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (var field in fields) {
+                ObserveFieldAttribute attribute = field.GetCustomAttribute<ObserveFieldAttribute>();
+                if (attribute != null) {
+                    tempList.Add(new ObservedFieldData(field, observedObject, attribute.expectedValue, attribute.expectedValuetype));
+                }
+            }
+            return new ObservedObjectData(name, objectType, tempList);
+        }
+
+
+    }
+
+    public class ObservedFieldData {
+        public readonly FieldInfo Info;
+        public readonly object ReflectedObject;
+        public readonly object ExpectedValue;
+        public readonly Type ExpectedValueType;
+
+
+        public ObservedFieldData(FieldInfo info, object reflectedObject, object expectedValue = null, Type expectedValueType = null) {
+            Info = info;
+            ReflectedObject = reflectedObject;
+            ExpectedValue = expectedValue;
+            ExpectedValueType = expectedValueType;
+        }
+    }
+
+    public class ObservedObjectData {
+        public readonly string Name;
+        public readonly Type ObjectType;
+        public readonly List<ObservedFieldData> Fields;
+
+        public ObservedObjectData(string name, Type objectType, List<ObservedFieldData> fields) {
+            Name = name;
+            ObjectType = objectType;
+            Fields = fields;
+        }
+    }
+}
